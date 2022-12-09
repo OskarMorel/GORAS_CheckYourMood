32d51988b43524de28817f813f2601857ba80802
Simon Launay <80904336+SimonLAUNAY@users.noreply.github.com>
2022-12-09 08:36:44 +0100

    Add files via upload

---
+++ yasmf/fileshelpers.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * imagehelpers.php
+ * @author Equipe 6 | Gauthier Guirola-Boe , Simon Launay , Tatiana Borgi , Gabriel Benniks-Chabbert
+ * @SyndicSaas 2021-2022
+ */
+
+namespace yasmf;
+
+use yasmf\config;
+
+/**
+ * Permet l'upload d'images
+ * @package yasmf
+ */
+class fileshelpers
+{
+
+	/**
+	 * Uploade sur le site un fichier
+	 * @param $nomFichier nom du fichier à ajouter
+	 * @param $chemin chemin d'accès
+	 * @param $taille taillede l'image
+	 * @param $extension extention de l'image
+	 * @return string|null  true si ajout effectué, false sinon (erreur serveur ou informations non valides)
+	 */
+	public static function upload_file($nomFichier, $chemin, $taille, $extension)
+	{
+		$dossier = "files/";
+		$taille_maxi = 10000000000;
+		$extensions = array('.png', '.gif', '.jpg', '.jpeg', 'pdf', 'docx', 'doc');
+		//Début des vérifications de sécurité...
+
+		if ($taille > $taille_maxi) {
+			$erreur = 'Le fichier est trop gros...';
+		}
+		if (!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+		{
+			//On formate le nom du fichier ici...
+			$nomFichier = strtr(
+				$nomFichier,
+				'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+				'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy'
+			);
+			$nomFichier = preg_replace('/([^.a-z0-9]+)/i', '-', $nomFichier);
+			if (move_uploaded_file($chemin, $dossier . $nomFichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+			{
+				//echo 'Upload effectué avec succès !';
+				$nomFichierNew = self::generateName() . $extension;
+				rename("files/" . $nomFichier, "files/" . $nomFichierNew);
+				return $nomFichierNew;
+			} else //Sinon (la fonction renvoie FALSE).
+			{
+				//echo 'Echec de l\'upload !';
+				return null;
+			}
+		} else {
+			echo $erreur;
+		}
+	}
+
+
+	/**
+	 * Génère un nom de fichier aléatoirement (unique) de 8 caractères
+	 * @return string nom  généré
+	 */
+	public static function generateName()
+	{
+		$carPossibles = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890";
+		$nbCarPossibles = strlen($carPossibles) - 1;
+		$name = "";
+		for ($i = 0; $i < 8; $i++) {
+			$pos = mt_rand(0, $nbCarPossibles);
+			$name .= $carPossibles[$pos];
+		}
+		return $name;
+	}
+}
---
+++ yasmf/router.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ *     yasmf - Yet Another Simple MVC Framework (For PHP)
+ *     Copyright (C) 2019   Franck SILVESTRE
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace yasmf;
+
+use Exception;
+use yasmf\controllers;
+
+class router {
+    public function route($dataSource)
+    {
+        try {
+            $controllerName = httphelper::getParam('controller') ?: 'home';
+
+            $controllerQualifiedName = "controllers\\" . $controllerName . "Controller";
+
+            try {
+                $controller = new $controllerQualifiedName();
+
+                // set the action to trigger
+                $action = httphelper::getParam('action') ?: 'index';
+                // trigger the appropriate action and get the resulted view
+                $view = $controller->$action($dataSource->getPdo());
+
+                // render the view
+                try {
+                    $view->render();
+                } catch (\Error $e) {
+                    header("Location: /?controller=erreur&err=view");
+                }
+            } catch (\Error $e) {
+                header("Location: /?controller=erreur&err=controller");
+            }
+        } catch (\Error $e) {
+            header("Location: /?controller=erreur&err=router");
+        }
+
+    }
+
+}
